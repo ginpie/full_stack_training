45f1b9c962a08446eeedc26ba9213270ab970047
chen <chenjp1989@gmail.com>
2020-07-28 15:23:58 +1000

    create

---
+++ express-assignment/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+// jsonfile package, w/r json file
+const jsonfile = require("jsonfile");
+const currentPath = process.cwd();
+const file = `${currentPath}/people.json`;
+console.log(file);
+
+app.get("/people", (req, res, next) => {
+  const { name } = req.query;
+  jsonfile
+    .readFile(file)
+    .then((obj) => {
+      const size = Object.keys(obj).length;
+      // get all people
+      if (!size) {
+        res.send("no person ever created");
+      }
+      if (name) {
+        Object.values(obj).forEach((values) => {
+          console.log(values.name, name);
+          console.log(values.name === name);
+          if (values.name == name) {
+            res.send(`person found: ${name}`);
+          }
+        });
+        res.send("person not found");
+      }
+      // get a person by name
+      else {
+        console.dir(obj);
+        console.log(`There are ${size} people in total.`);
+        res.send(obj);
+      }
+    })
+    .catch((error) => console.error(error));
+});
+
+// create a person
+app.post("/people/:name", (req, res, next) => {
+  const { name } = req.params;
+  const obj = { name: name };
+  jsonfile
+    .readFile(file)
+    .then((obj) => {
+      people = obj;
+      const size = Object.keys(obj).length;
+      obj[size + 1] = { name: name };
+      jsonfile
+        .writeFile(file, obj)
+        .then((res) => {
+          console.log(obj);
+          console.log("Write complete");
+        })
+        .catch((error) => console.error(error));
+    })
+    .catch((error) => console.error(error));
+
+  res.send(`Person created: ${name}`);
+});
+
+app.delete;
+("/people/:name");
+
+app.listen(3000);
